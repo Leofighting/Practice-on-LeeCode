4e327eed3d05c173fc874a2e468028edea0a53c8
leoxiaojw <leoxiaojw@gmail.com>
2020-05-15 08:56:36 +0800

    update on 05-15

---
+++ client.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+__author__ = "leo"
+
+import requests
+
+
+def send_request(url):
+    r = requests.get(url)
+    return r.status_code
+
+
+def visit_baidu():
+    return send_request("http://www.baidu.com")
