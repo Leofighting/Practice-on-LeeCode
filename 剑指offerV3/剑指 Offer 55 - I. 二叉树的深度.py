0693da4c0e85ee3d357adb9c885d769176fa1581
leoxiaojw <leoxiaojw@gmail.com>
2020-06-24 07:10:00 +0800

    update on 06-24

---
+++ 剑指offerV3/剑指 Offer 55 - I. 二叉树的深度.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+__author__ = "leo"
+
+
+# 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+class Solution1:
+    def max_depth(self, root):
+        if not root:
+            return 0
+        queue, res = [root], 0
+        while queue:
+            tmp = []
+            for node in queue:
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            queue = tmp
+            res += 1
+        return res
