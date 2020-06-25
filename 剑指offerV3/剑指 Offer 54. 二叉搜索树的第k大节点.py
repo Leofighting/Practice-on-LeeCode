0736e5cca9bfce483b3a224f0df8374f3c233780
leoxiaojw <leoxiaojw@gmail.com>
2020-06-26 06:41:12 +0800

    update on 06-26

---
+++ 剑指offerV3/剑指 Offer 54. 二叉搜索树的第k大节点.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+__author__ = "leo"
+
+
+class Solution:
+    def kth_largest(self, root, k):
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.right)
+            if self.k == 0:
+                return
+            self.k -= 1
+            if self.k == 0:
+                self.res = root.val
+            dfs(root.left)
+
+        self.k = k
+        dfs(root)
+        return self.res
